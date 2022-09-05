626482a4154581133f633614ce7c0a61005dfe27
kkanto <108447856+kkanto@users.noreply.github.com>
2022-09-05 16:26:18 +0200

    Create maven project

---
+++ src/main/java/de/allianz/ERERERERER/ErererererApplication.java
@@ -0,0 +1,13 @@
+package de.allianz.ERERERERER;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErererererApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErererererApplication.class, args);
+	}
+
+}
